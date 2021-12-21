e3be66b6619a249ec725338eba35e409678a1d5f
kingqabil <kingqabil@live.com>
2021-12-21 20:24:53 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,11 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
   devServer: {
     static: './dist',
   },
@@ -12,16 +17,15 @@ module.exports = {
       template: './src/index.html',
     }),
   ],
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
       },
     ],
   },
